de98e076369a4e6133a91402f6d82928e785c4f7
Sergey <Sergey@SERGEY->
2020-04-17 19:32:14 +0300

    10 polygons, wrong coordinates start for next poly

--- Turtle_1.py
+++ Turtle_1.py
@@ -3,9 +3,10 @@ import turtle
 turtle.shape('turtle')
 turtle.speed(0)
 
-for i in range(1, 1000, 1):
-    turtle.forward(i*2)
-    turtle.left(90)
+# # Square spiral drawing
+# for i in range(1, 1000, 1):
+#     turtle.forward(i*2)
+#     turtle.left(90)
 
 # 10 Square drawing
 # for i in range(10, 100, 10):
@@ -22,5 +23,30 @@ for i in range(1, 1000, 1):
 #     turtle.pendown()
 
 
+# # Spiral drawing
+# k = 5
+# fi_rad = 0.1
+# fi_degr = fi_rad * (90 / 3.14)
+# for i in range(0, 1000):
+#     ro = k * fi_rad
+#     turtle.forward(ro)
+#     turtle.left(fi_degr)
+#     fi_rad += 0.1
+#     ro += ro
+
+# Multi Polygon drawing
+a = 50
+b = 90
+sides = 3
+for it in range(1, 10, 1):
+    for i in range(sides):
+        turtle.forward(a)
+        z = 360 / sides
+        turtle.left(z)
+    turtle.penup()
+    turtle.setpos(-a, 0)
+    turtle.pendown()
+    a += 5
+    sides += 1
 
 turtle.exitonclick()
